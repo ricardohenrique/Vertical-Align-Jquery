06c845a106dd603fe6e9ae8b5eec5f48517a3ac4
ricardohenrique <ricardo.hb@outlook.com>
2016-05-16 16:07:26 -0300

    Edição dia 16/05

--- main.js
+++ main.js
@@ -1,20 +1,16 @@
 $(function(){
-
     // Nav Fixed
     var widthWindow = $(window).width() + 17;
     
-    // Align thumbs
     $(window).load(function(){
-        centerContentFirst();
-        centerContentSecond();
+        alignVertical();
     });
 
     $(window).resize(function(){
-        centerContentFirst();
-        centerContentSecond();
+        alignVertical();
     });
 
-    function centerContentFirst(){
+    function alignVertical(){
         var container = $('.container-align');
         var altura_margin = ((container_01.height() / 2) - 15); 
         $('.vertical-align').css('margin-top', (altura_margin) + 'px');
